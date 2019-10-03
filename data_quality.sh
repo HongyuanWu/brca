4d39821159f71f8f4eb4c394d6699950d3c77c57
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-10-02 23:34:49 -0500

    Create data_quality.sh

---
+++ data_quality.sh
@@ -0,0 +1,9 @@
+wget https://raw.githubusercontent.com/Shicheng-Guo/Gscutility/master/cov2matrix.pl
+perl cov2matrix.pl > brca.txt
+data<-read.table("brca.txt",head=T,check.names=F)
+nna<-apply(data,1,function(x) sum(is.na(x)))/ncol(data)
+input<-data[-which(nna>0.5),]
+nna<-apply(input,2,function(x) sum(is.na(x)))/nrow(input)
+sort(nna)
+input$BN190199
+input$BN190073
