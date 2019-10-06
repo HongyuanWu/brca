95ca77a2b7f8ff14564e9b61d01bd91327cc92ca
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-10-05 22:27:17 -0500

    Update data_quality.sh

--- data_quality.sh
+++ data_quality.sh
@@ -12,3 +12,16 @@ names(nna[nna>0.3])
 write.table(names(nna[nna>0.3]),file="high.missing.sample.txt",sep="\t",quote=F)
 input$BN190199
 input$BN190073
+
+
+data<-read.table("brca.txt",head=T,check.names=F)
+CHR<-unlist(lapply(strsplit(rownames(data),":"),function(x) x[1]))
+POS<-as.numeric(unlist(lapply(strsplit(rownames(data),":"),function(x) x[2])))
+START<-POS-150
+END<-POS+150
+input<-data.frame(CHR,START,END)
+write.table(input,file="brca.hg19.bed",sep="\t",quote=F,col.names=F,row.names=F)
+
+cd ~/hpc/methylation/brca/19B0731C_MethylTarget/methyfreq
+bedtools sort -i brca.hg19.bed > brca.hg19.sort.bed
+bedtools merge -i brca.hg19.sort.bed > brca.hg19.sort.merge.bed
