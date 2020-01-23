9c5722d036861f15e4583a2f89cc7e59e8f0eb71
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-22 21:41:17 -0600

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+setwd("/home/guosa/hpc/methylation/brca/meta")
+memo="breastcancer"
+data<-read.csv("https://raw.githubusercontent.com/Shicheng-Guo/esophageal/master/extdata/ESCC-UKBB-RSKAT2020.csv",as.is=T)
+data<-read.csv("https://raw.githubusercontent.com/Shicheng-Guo/colon/master/extdata/UKBB50K/CRC-UKBB-RSKAT2020.csv",as.is=T)
+data<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/housekeeping/master/housekeeping.txt",as.is=T,head=T,sep="\t")
+data<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/cholangiocarcinoma/master/cholTargetRegion50.txt",as.is=T)
+data<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/brca/master/extdata/brca.tcga.target.hg19.bed",as.is=T)
+head(data)
+symbol<-data[,4]
+source("https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/PancancerMetaDge.R")
+source("https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/pancancermetaOSHR.R")
+source("https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/OROSmerge.R")
+pancancermetadge(symbol,memo)
+pancancermetaOSHR(symbol,memo)
+OROSmerge(memo)
